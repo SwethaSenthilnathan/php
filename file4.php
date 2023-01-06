190180103a511003141d7c4af884b04d97373d9a
SwethaSenthilnathan <122120144+SwethaSenthilnathan@users.noreply.github.com>
2023-01-07 00:30:42 +0530

    Add files via upload

---
+++ file4.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<style>
+body{
+    background-image:url('register2.jpg');
+    background-repeat:repeat;
+    background-size:100%;
+}
+</style>
+</head>
+<body>
+    
+
+
+<form action="file5.php" method="post">
+<font size="4" face="verdana"color="white">First Name: <br><input type="text" name="firstname"><br></font>
+<font size="4" face="verdana"color="white">Last name: <br><input type="text" name=lastname"><br></font>
+<font size="4" face="verdana"color="white">Gender: <br><input type="text" name="gender"><br></font>
+<font size="4" face="verdana"color="white">DOB:<br> <input type="date" name="dob"><br></font>
+<font size="4" face="verdana"color="white">Age:<br> <input type="number" name="age"><br></font>
+<font size="4" face="verdana"color="white">Qualification:<br> <input type="text" name="qualification"><br></font>
+<font size="4" face="verdana"color="white">Contact:<br> <input type="number" name="contact"><br></font>
+<font size="4" face="verdana"color="white">Emailid:<br> <input type="text" name="emailid"><br></font>
+<font size="4" face="verdana"color="white">password:<br> <input type="text" name="password"><br></font>
+<font size="4" face="verdana"color="white">address:<br> <input type="text" name="address"><br></font>
+<font size="4" face="verdana"color="white">College name: <br><input type="text" name="college_name"><br></font>
+<font size="4" face="verdana"color="white">Department name:<br> <input type="text" name="dept_name"><br></font>
+<font size="4" face="verdana"color="white">Fee status: <br><input type="text" name="fee_status"><br></font>
+<font size="4" face="verdana"color="white">Event id: <br><input type="number" name="e_id"><br></font>
+
+
+	
+<input type ="submit">
+
+
+</form>
+
+</body>
+</html>
